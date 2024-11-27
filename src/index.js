87d8f7cbbdc593ee0343f3db205652623eb08f1a
kinjirou2024 <quangpvintern@gmail.com>
2024-11-27 08:51:27 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
 
 app.use("/api", require("./routes"));
-
+// app
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
